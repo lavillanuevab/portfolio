eb0e48fe2d14a0921ad298df4e025ed8faec9c17
Leonardo Villanueva <villanueva.leonardo@inventec.com>
2024-10-22 23:55:49 -0600

    Updates

--- assets/ContactView-CudSuzKy.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c,a as o}from"./index-B5fTUIeY.js";const n={},s=o("h1",null,"Contact",-1),a=[s];function _(r,l){return t(),c("div",null,a)}const i=e(n,[["render",_]]);export{i as default};
---
+++ assets/ContactView-DNzHE8qc.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c,a as o}from"./index-BN4TcJG5.js";const n={},s=o("h1",null,"Contact",-1),a=[s];function _(r,l){return t(),c("div",null,a)}const i=e(n,[["render",_]]);export{i as default};
